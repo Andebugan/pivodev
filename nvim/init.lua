71a169cfafa29ba1341a78e4be3d49762128c76f
andebugan <andreibugaenko2012@mail.ru>
2024-06-04 21:57:21 +0000

    changed folding method

--- nvim/init.lua
+++ nvim/init.lua
@@ -17,6 +17,8 @@ vim.o.smartcase = true            -- set smart case for search
 
 vim.opt.fillchars = { eob = " " } -- disable tilde symbols after EOF
 
+vim.o.foldmethod = "indent"       -- fold
+
 -- key mappings
 vim.g.mapleader = ' ' -- set space as the leader key
 
